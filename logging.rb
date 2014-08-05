f7302bf7612328c7b1a50cc1ef9f3e8c1ab4e8da
dch <dch@dch.io>
2014-08-05 21:34:54 +0000

    first commit

---
+++ logging.rb
@@ -0,0 +1,28 @@
+module Logging
+
+	def info(msg)
+		return Logger.instance.out(msg, "INFO")
+	end
+	def warn(msg)
+		return Logger.instance.out(msg, "WARN")
+	end
+	def error(msg)
+		return Logger.instance.out(msg, "ERROR")
+	end
+
+	require 'singleton'
+  class Logger
+    include Singleton
+
+    def initialize
+      @log = File.open("LOG_PATH", "a")
+    end
+
+    def out(msg, level)
+			@log.puts("#{level[0]}, [#{Time.now}]  #{level} -- : #{msg}")
+			@log.flush
+    end
+
+  end
+
+end
